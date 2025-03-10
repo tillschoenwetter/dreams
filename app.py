1a937ea59e493e0dd8bfddba7d797af54843a8e9
tillschoenwetter <till.schoenwetter@gmail.com>
2025-03-10 01:28:06 +0100

    port fix

--- app.py
+++ app.py
@@ -127,6 +127,5 @@ def index():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # default is 10000 if not set
     app.run(host="0.0.0.0", port=port)
-
